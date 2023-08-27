416c250d2b368f131d378d6f2154a2c178063173
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-08-27 23:55:02 +0600

    main page added

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Hero_Section from "@/components/Hero_Section"
+import PopularGame from "@/components/PopularGame"
+
+
+
+export default function page() {
+  return (
+    <>
+
+      <main>
+        <Hero_Section />
+        <PopularGame />
+      </main>
+    </>
+  )
+}
