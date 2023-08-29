3ac472eb9d93ad68c8454797ce9659554b9f50e8
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-08-29 23:03:41 +0600

    token/jwt/cookie send when login

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -2,6 +2,7 @@ import userCollection from '@/models/userModels'
 import { connect } from '@/db/db'
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 connect()
 
@@ -12,7 +13,26 @@ export async function POST(req) {
         if (!findUser) return NextResponse.json({ error: "Invalid User Data", status: 400 }, { status: 400 });
         const matchPassword = await bcrypt.compare(password, findUser.password)
         if (!matchPassword) return NextResponse.json({ error: "Invalid User Data", status: 401 }, { status: 401 });
-        return NextResponse.json({ message: "Login successful. Redirecting to Home", status: 200 }, { status: 200 });
+        const tokenPayload = {
+            _id: findUser._id,
+            name: findUser.name,
+            email: findUser.email,
+            img: findUser.img || null
+        }
+        const token = await jwt.sign(tokenPayload, process.env.JWT_SECRET)
+        const expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth() + 1);
+        const response = NextResponse.json({
+            message: "Login successful. Redirecting to Home",
+            status: 200
+        }, {
+            status: 200
+        });
+        response.cookies.set('gamezonetoken', token, {
+            // httpOnly: true,
+            // expires: expirationDate,
+        })
+        return response;
     } catch (error) {
         return NextResponse.json({ error: "Internal server error", status: 500 }, { status: 500 })
     }
