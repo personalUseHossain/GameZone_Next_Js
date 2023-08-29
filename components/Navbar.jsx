53e7b4727ce1a64dcc195bcc1e73e2d0e7c112ae
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-08-29 23:01:01 +0600

    context added and others

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
 "use client";
+import "@/public/CSS/Navbar.css";
 import Link from "next/link";
 import Image from "next/image";
-import "@/public/css/Navbar.css";
+
+//check if user authenticated or not
+// import { verifyToken } from "@/utils/verifyToken";
 
 //images
 import logo from "@/public/images/logo.png";
 import user from "@/public/images/user.jpg";
+
+import { MyContext } from "@/app/layout";
+
+import { isAuth, signout, getUserData } from "@/utils/auth";
 
 //font awesome
 import {
@@ -14,24 +21,58 @@ import {
   faBarsStaggered,
   faXmark,
   faArrowLeft,
+  faBookmark,
+  faArrowRightFromBracket,
+  faMessage,
+  faCircleQuestion,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify"; //for alert/message
+import "react-toastify/dist/ReactToastify.css"; //for alert/message css
+import { useRouter } from "next/navigation"; //for redirect user to login page
+
+async function checkToken() {
+  const check = await isAuth();
+  return check;
+}
 
 export default function Navbar() {
+  let menuHeight = { height: "" };
+  const router = useRouter();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [openMenu, setOpenMenu] = useState(false);
-  const login = false;
+  const { login, setLogin } = useContext(MyContext);
+  const [userData, setUserData] = useState({ userData: null, status: false });
   function toggleMenu() {
     setIsMenuOpen(!isMenuOpen);
+    if (!isMenuOpen) {
+      document.querySelector(".menu-user").style.height = "100vh";
+      document.body.style.overflow = "hidden";
+    } else {
+      document.querySelector(".menu-user").style.height = "";
+    }
   }
-  let menuStyle = { height: "0" };
-  if (typeof window !== "undefined" && window.innerWidth < 950) {
-    isMenuOpen ? (menuStyle.height = "100vh") : (menuStyle.height = "0");
+  function logout() {
+    signout();
+    setLogin(false);
+    toast.success("Logged out");
+    router.push("/");
   }
+  useEffect(() => {
+    async function fetchData() {
+      const userData = await getUserData();
+      if (userData.status) {
+        setUserData(userData);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <nav>
+      <ToastContainer />
       {!openMenu ? (
         <>
           <div className="logo-menu">
@@ -74,7 +115,7 @@ export default function Navbar() {
               icon={isMenuOpen ? faXmark : faBarsStaggered}
             />
           </div>
-          <div className="menu-user" style={menuStyle}>
+          <div className="menu-user" style={menuHeight}>
             <div className="menu">
               <Link href={"/"}>Home</Link>
               <Link href={"/games"}>Games</Link>
@@ -82,7 +123,10 @@ export default function Navbar() {
               <Link href={"/contact"}>Contact</Link>
             </div>
           </div>
-          <div className="user">
+          <div
+            className="user"
+            style={{ marginRight: "5rem", cursor: "pointer" }}
+          >
             {!login ? (
               <>
                 <div className="links_big">
@@ -101,7 +145,54 @@ export default function Navbar() {
               </>
             ) : (
               <>
-                <Image src={user} width={40} height={40} alt="User" />
+                <Image
+                  onClick={() => {
+                    document.querySelector(
+                      ".user-content-container"
+                    ).style.display = "block";
+                  }}
+                  src={user}
+                  width={40}
+                  height={40}
+                  alt="User"
+                />
+                <div className="user-content-container">
+                  <div className="user-top">
+                    <div className="user-top-left">
+                      <Image src={user} width={40} height={40} alt="User" />
+                      <FontAwesomeIcon
+                        icon={faXmark}
+                        onClick={() => {
+                          document.querySelector(
+                            ".user-content-container"
+                          ).style.display = "none";
+                        }}
+                      />
+                    </div>
+                    <div className="user-top-right">
+                      <h3>Muhammad Hossain</h3>
+                      <small>personal.mdhossain@gmail.com</small>
+                    </div>
+                  </div>
+                  <div className="user-bottom">
+                    <div className="user-bottom-content">
+                      <FontAwesomeIcon icon={faBookmark} />
+                      <h4>Saved Games</h4>
+                    </div>
+                    <div onClick={logout} className="user-bottom-content">
+                      <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                      <h4>Sign out</h4>
+                    </div>
+                    <div className="user-bottom-content">
+                      <FontAwesomeIcon icon={faMessage} />
+                      <h4>Send Feedback</h4>
+                    </div>
+                    <div className="user-bottom-content">
+                      <FontAwesomeIcon icon={faCircleQuestion} />
+                      <h4>Support</h4>
+                    </div>
+                  </div>
+                </div>
               </>
             )}
           </div>
