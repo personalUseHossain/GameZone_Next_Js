8c5fc489746b2060e49feb5e0546fb4d04dc1b77
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-09-15 09:33:52 +0600

    single game card

---
+++ components/SingleGame.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SingleGame(props) {
+  return props.games.map((game) => {
+    return (
+      <>
+        <div className="game" key={game._id}>
+          <Link
+            style={{ textDecoration: "none", color: "black" }}
+            href={`/game/${game._id}`}
+          >
+            <Image
+              src={"/uploads/" + game.img[0]}
+              height={150}
+              width={250}
+              alt="404"
+            />
+            <h4>{game.name}</h4>
+          </Link>
+        </div>
+      </>
+    );
+  });
+}
