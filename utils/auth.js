8071b607e372073f6351ff5d4233eb80c47a3890
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-09-16 11:42:09 +0600

    is auth verify token updated to authentiate user

--- utils/auth.js
+++ utils/auth.js
@@ -1,20 +1,22 @@
 import Cookies from 'universal-cookie'; // for get cookies
 import jwt from 'jsonwebtoken'; // to sign, verify, decode jwt
 
-
 const cookies = new Cookies(); // to get cookies
 
 // to check if user is authenticated or not
-export function isAuth() {
+export async function isAuth() {
     const token = cookies.get('gamezonetoken');
-    if (token) {
-        // console.log(jwt.verify(token, "helloworldhowareyouiamhossainandiliketowritecode"))
-        const data = jwt.decode(token);
-        if (data) return true;
-        return false;
-    }
+    if (!token) return false
+    const req = await fetch('/api/auth', {
+        method: "POST",
+        body: JSON.stringify(token),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const res = await req.json();
+    if (res.success) return true;
     return false
-
 }
 
 //signout function
