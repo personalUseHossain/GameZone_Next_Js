590586d806a26cd35d61c473be9e49e59447bc85
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-09-21 11:50:00 +0600

    domain changed

--- middleware.js
+++ middleware.js
@@ -7,17 +7,17 @@ export async function middleware(req) {
     const token = req.cookies.get('gamezonetoken')?.value || null;
     const path = req.nextUrl.pathname;
     if (token && ['/login', '/signup'].includes(path)) {
-        return NextResponse.redirect('http://localhost:3000');
+        return NextResponse.redirect('https://gamezone-d9lyq1q4n-personalusehossain.vercel.app/');
     }
     if (!token && path.startsWith('/game')) {
         const redirect = path
-        return NextResponse.redirect(`http://localhost:3000/login`);
+        return NextResponse.redirect(`https://gamezone-d9lyq1q4n-personalusehossain.vercel.app//login`);
     }
 
     if (path.startsWith('/dashboard')) {
         const userData = await getUserData(token);
         if (userData.isAdmin === undefined) {
-            return NextResponse.redirect('http://localhost:3000');
+            return NextResponse.redirect('https://gamezone-d9lyq1q4n-personalusehossain.vercel.app/');
         }
     }
     return NextResponse.next();
