fe1d273d1cc346f9c0308e68c9b7f04b47164c87
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-09-21 10:56:44 +0600

    mongoose timeout changed and search  added

--- app/api/games/route.js
+++ app/api/games/route.js
@@ -5,20 +5,20 @@ import { NextResponse } from "next/server";
 connect()
 
 export async function GET(req) {
-    // const searchparam = req.nextUrl.searchParams.get('search');
-    // if (searchparam) {
-    //     // Create a regular expression pattern to search for the keyword
-    //     const keywordRegex = new RegExp(searchparam, 'i'); // 'i' makes it case-insensitive
+    const searchparam = req.nextUrl.searchParams.get('search');
+    if (searchparam) {
+        // Create a regular expression pattern to search for the keyword
+        const keywordRegex = new RegExp(searchparam, 'i'); // 'i' makes it case-insensitive
 
-    //     // Use $regex to perform a case-insensitive search on the "keyword" field
-    //     const games = await gameCollection.find(
-    //         { keyword: { $regex: keywordRegex } },
-    //         { keyword: 0 } // Exclude the "keyword" field from the results
-    //     );
-    //     console.log(games)
-    //     return NextResponse.json({ message: 'success', data: games });
-    // }
-    let games = await gameCollection.find(); // Exclude the "keyword" field
+        // Use $regex to perform a case-insensitive search on the "keyword" field
+        const games = await gameCollection.find(
+            { keyword: { $regex: keywordRegex } },
+            { keyword: 0 } // Exclude the "keyword" field from the results
+        );
+        console.log(games)
+        return NextResponse.json({ message: 'success', data: games });
+    }
+    let games = await gameCollection.find({}, { keyword: 0 }); // Exclude the "keyword" field
     return NextResponse.json({ message: 'success', data: games });
 }
 
